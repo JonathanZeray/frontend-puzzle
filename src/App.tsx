a2bd8831e522446f189ea60a623fe99668d3328c
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-11-19 21:40:33 +0100

    Implemented tile grid with basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
+const ROWS = 4;
+const COLUMNS = 4;
+
+const generateSolvedBoard = () => {
+  const totalTiles = ROWS * COLUMNS;
+  return Array.from({ length: totalTiles }, (_, i) => i + 1);
+};
+
 function App() {
+  const [board, setBoard] = useState(generateSolvedBoard());
+
   return (
     <>
-      <div className="container">N-Puzzle</div>;
+      <div className="container">
+        <div className="tile-grid">
+          {board.map((tile) => (
+            <div
+              key={tile}
+              className={`tile ${tile === 16 ? "empty-tile" : ""}`}
+            >
+              {tile !== 16 && tile}
+            </div>
+          ))}
+        </div>
+      </div>
+      ;
     </>
   );
 }
