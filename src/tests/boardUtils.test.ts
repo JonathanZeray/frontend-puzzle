3c2b0f783f7f4470e0d3744693301e84b1edf0b7
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-11-20 19:12:01 +0100

    Test: basic tests for getValidMoves function to verify valid and unvalid moves.

--- src/tests/boardUtils.test.ts
+++ src/tests/boardUtils.test.ts
@@ -1,4 +1,4 @@
-import { isSolved } from "../utils/boardUtils";
+import { COLUMNS, getValidMoves, isSolved } from "../utils/boardUtils";
 
 describe("boardUtils tests", () => {
   test("isSolved detects a solved board", () => {
@@ -7,7 +7,17 @@ describe("boardUtils tests", () => {
   });
 
   test("isSolved detects a non solved board", () => {
-    const notSolvedBoard = [1, 3, 2, 4, 5, 6, 7, 8];
+    const notSolvedBoard = [2, 3, 1, 4, 6, 5, 7, 8];
     expect(isSolved(notSolvedBoard)).toBe(false);
+  });
+
+  test("getValidMoves returns correct moves for a top-left corner index", () => {
+    const validMoves = [1, COLUMNS]; // Hö & ner
+    expect(getValidMoves(0)).toEqual(validMoves);
+  });
+
+  test("getValidMoves does not include invalid moves", () => {
+    const validMoves = getValidMoves(1);
+    expect(validMoves).not.toContain(3); // indexposition 3 ska inte vara en valid move från indexposition 1.
   });
 });
